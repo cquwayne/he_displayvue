3be430df34f8dad0998816874b6f2753ac11ddcb
ethanworld <742643715@qq.com>
2020-04-17 01:17:14 +0800

    0417

--- src/components/integration/Influx.vue
+++ src/components/integration/Influx.vue
@@ -1,13 +1,85 @@
 <template>
-  <div class="Influx">
-
-  </div>
+  <el-container class="Influx">
+    <el-header>
+      <el-row>
+        <el-col :span="6">
+          <el-select v-model="query.processId" filterable placeholder="请选择">
+            <el-option
+              v-for="item in processList" :key="item.index"
+              :label="item.title"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="handleQuery">
+            查询
+          </el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <el-divider></el-divider>
+      <el-table
+        :data="table"
+        style="width:100%">
+        <el-table-column label="时间">
+          <template slot-scope="scope">
+            {{ scope.row.time }}
+          </template>
+        </el-table-column>
+        <el-table-column label="坐标">
+          <template slot-scope="scope">
+            {{ scope.row.pointId }}
+          </template>
+        </el-table-column>
+        <el-table-column label="属性">
+          <template slot-scope="scope">
+            {{ scope.row.attributeId }}
+          </template>
+        </el-table-column>
+        <el-table-column label="数据值">
+          <template slot-scope="scope">
+            {{ scope.row.value }}
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
 </template>
 <script>
 export default {
-  name: 'Influx'
+  name: 'Influx',
+  data () {
+    return {
+      url: this.$store.state.url,
+      processList: [],
+      query: {
+        processId: null
+      },
+      table: []
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.$axios.get(vm.url + 'integration/processs').then(res => {
+        vm.processList = res.data
+      })
+    })
+  },
+  methods: {
+    handleQuery () {
+      this.$axios.get(this.url + 'data', this.query).then(res => {
+        this.table = res.data
+      })
+    }
+  }
 }
 </script>
 <style scoped lang="scss">
-
+  .Influx{
+    .el-header{
+      padding: 20px!important;
+    }
+  }
 </style>
